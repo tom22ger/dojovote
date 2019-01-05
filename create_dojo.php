4e3fdc0470ac83e8096b27942eb8cf6e2fb72b2f
tom22ger <thomasadrian@mac.com>
2019-01-04 20:11:13 -0500

    azure sql connection

--- create_dojo.php
+++ create_dojo.php
@@ -1,11 +1,28 @@
 <?php 
-
+/*
 $servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "dojo_vote";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
+*/
+
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:dojovote.database.windows.net,1433; Database = dojovote", "tomger", "Oodles0fDojos4me!");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "tomger@dojovote", "pwd" => "Oodles0fDojos4me!", "Database" => "dojovote", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:dojovote.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 
 //echo "Connected successfully";
 
